d458c83465331fe02804d82b56aeb002492a7178
haldomae <hal.domae@gmail.com>
2026-05-18 17:03:31 +0900

    APIServiceを追加

---
+++ app/src/main/java/com/example/weatherapitest/WeatherApiService.kt
@@ -0,0 +1,17 @@
+package com.example.weatherapitest
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+// このAPIにはこうゆうリクエストを送るという仕様を定義する
+interface WeatherApiService {
+    // このURLにGETリクエストを送る
+    @GET("v1/forecast")
+    fun getCurrentWeather(
+        // URLの?キー =値 部分(クエリーパラメーター)を指定
+        @Query("latitude") latitude: Double,
+        @Query("longitude") longitude: Double,
+        @Query("current_weather") currentWeather: Boolean = true
+    ): String
+
+}
